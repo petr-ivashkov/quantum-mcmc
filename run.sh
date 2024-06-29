7190e195590b6b78d3394d8a7c8f25036b75420a
Ivashkov Petr <ivashkov.peter@gmail.com>
2024-06-29 09:10:08 +0200

    switched to miniconda

--- run.sh
+++ run.sh
@@ -2,19 +2,21 @@
 
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=48
-#SBATCH --time=20:00:00
-#SBATCH --job-name="phase_diagram_quantum_SK_n5_10"
+#SBATCH --time=04:00:00
+#SBATCH --job-name="bo_cluster_TEST"
 #SBATCH --mem-per-cpu=1024
 
 export OMP_NUM_THREADS=48;
 
-source /cluster/home/pivashkov/virtualenvs/qmcmc/bin/activate
+conda activate qmcmc
 
-papermill ./phase_transition/phase_diagram_quantum_SK_n5.ipynb ./phase_transition/phase_diagram_quantum_SK_n5.ipynb
-papermill ./phase_transition/phase_diagram_quantum_SK_n6.ipynb ./phase_transition/phase_diagram_quantum_SK_n6.ipynb
-papermill ./phase_transition/phase_diagram_quantum_SK_n7.ipynb ./phase_transition/phase_diagram_quantum_SK_n7.ipynb
-papermill ./phase_transition/phase_diagram_quantum_SK_n8.ipynb ./phase_transition/phase_diagram_quantum_SK_n8.ipynb
-papermill ./phase_transition/phase_diagram_quantum_SK_n9.ipynb ./phase_transition/phase_diagram_quantum_SK_n9.ipynb
-papermill ./phase_transition/phase_diagram_quantum_SK_n10.ipynb ./phase_transition/phase_diagram_quantum_SK_n10.ipynb
+papermill ./bo_cluster/reverse_annealing_bo_n4.ipynb ./bo_cluster/reverse_annealing_bo_n4.ipynb
 
-deactivate
+# papermill ./phase_transition/phase_diagram_quantum_SK_n5.ipynb ./phase_transition/phase_diagram_quantum_SK_n5.ipynb
+# papermill ./phase_transition/phase_diagram_quantum_SK_n6.ipynb ./phase_transition/phase_diagram_quantum_SK_n6.ipynb
+# papermill ./phase_transition/phase_diagram_quantum_SK_n7.ipynb ./phase_transition/phase_diagram_quantum_SK_n7.ipynb
+# papermill ./phase_transition/phase_diagram_quantum_SK_n8.ipynb ./phase_transition/phase_diagram_quantum_SK_n8.ipynb
+# papermill ./phase_transition/phase_diagram_quantum_SK_n9.ipynb ./phase_transition/phase_diagram_quantum_SK_n9.ipynb
+# papermill ./phase_transition/phase_diagram_quantum_SK_n10.ipynb ./phase_transition/phase_diagram_quantum_SK_n10.ipynb
+
+conda deactivate
