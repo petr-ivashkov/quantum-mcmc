18666b95b13d6470f84868507a5e272280bb516f
Ivashkov Petr <ivashkov.peter@gmail.com>
2024-07-01 18:38:18 +0200

    submitted jobs

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=12
-#SBATCH --time=10:00:00
-#SBATCH --job-name="scale_factors"
-#SBATCH --mem-per-cpu=4096
+#SBATCH --cpus-per-task=18
+#SBATCH --time=20:00:00
+#SBATCH --job-name="bo_cluster"
+#SBATCH --mem-per-cpu=1024
 
 export OMP_NUM_THREADS=48;
 
@@ -17,7 +17,7 @@ conda activate qmcmc
 # papermill ./bo_cluster/reverse_annealing_bo_n6.ipynb ./bo_cluster/reverse_annealing_bo_n6.ipynb
 # papermill ./bo_cluster/reverse_annealing_bo_n7.ipynb ./bo_cluster/reverse_annealing_bo_n7.ipynb
 # papermill ./bo_cluster/reverse_annealing_bo_n8.ipynb ./bo_cluster/reverse_annealing_bo_n8.ipynb
-# papermill ./bo_cluster/reverse_annealing_bo_n9.ipynb ./bo_cluster/reverse_annealing_bo_n9.ipynb
+papermill ./bo_cluster/reverse_annealing_bo_n9.ipynb ./bo_cluster/reverse_annealing_bo_n9.ipynb
 # papermill ./bo_cluster/reverse_annealing_bo_n10.ipynb ./bo_cluster/reverse_annealing_bo_n10.ipynb
 
 # papermill ./phase_transition/phase_diagram_quantum_SK_n4.ipynb ./phase_transition/phase_diagram_quantum_SK_n4.ipynb
@@ -28,7 +28,7 @@ conda activate qmcmc
 # papermill ./phase_transition/phase_diagram_quantum_SK_n9.ipynb ./phase_transition/phase_diagram_quantum_SK_n9.ipynb
 # papermill ./phase_transition/phase_diagram_quantum_SK_n10.ipynb ./phase_transition/phase_diagram_quantum_SK_n10.ipynb
 
-
+# papermill ./binder_cumulant/binder_cumulant_quantum_SK_n3.ipynb ./binder_cumulant/binder_cumulant_quantum_SK_n3.ipynb
 # papermill ./binder_cumulant/binder_cumulant_quantum_SK_n4.ipynb ./binder_cumulant/binder_cumulant_quantum_SK_n4.ipynb
 # papermill ./binder_cumulant/binder_cumulant_quantum_SK_n5.ipynb ./binder_cumulant/binder_cumulant_quantum_SK_n5.ipynb
 # papermill ./binder_cumulant/binder_cumulant_quantum_SK_n6.ipynb ./binder_cumulant/binder_cumulant_quantum_SK_n6.ipynb
@@ -36,9 +36,9 @@ conda activate qmcmc
 # papermill ./binder_cumulant/binder_cumulant_quantum_SK_n8.ipynb ./binder_cumulant/binder_cumulant_quantum_SK_n8.ipynb
 # papermill ./binder_cumulant/binder_cumulant_quantum_SK_n9.ipynb ./binder_cumulant/binder_cumulant_quantum_SK_n9.ipynb
 
-papermill ./scale_factors/scale_factors_random.ipynb ./scale_factors/scale_factors_random.ipynb
-papermill ./scale_factors/scale_factors_local.ipynb ./scale_factors/scale_factors_local.ipynb
-papermill ./scale_factors/scale_factors_quantum.ipynb ./scale_factors/scale_factors_quantum.ipynb
-papermill ./scale_factors/scale_factors_fixed_gamma.ipynb ./scale_factors/scale_factors_fixed_gamma.ipynb
+# papermill ./scale_factors/scale_factors_random.ipynb ./scale_factors/scale_factors_random.ipynb
+# papermill ./scale_factors/scale_factors_local.ipynb ./scale_factors/scale_factors_local.ipynb
+# papermill ./scale_factors/scale_factors_quantum.ipynb ./scale_factors/scale_factors_quantum.ipynb
+# papermill ./scale_factors/scale_factors_fixed_gamma.ipynb ./scale_factors/scale_factors_fixed_gamma.ipynb
 
 conda deactivate
